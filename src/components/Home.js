1b8d3db20f77623f2c609d409c86448fa48757b2
Johanna Salmi Sunnerdahl <73692594+Salmihanna@users.noreply.github.com>
2021-10-05 11:50:29 +0200

    add all file

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,13 +8,7 @@ const Home = () => {
     const [movies, setMovies] = useState([]);
     const API_KEY = process.env.REACT_APP_API_KEY;
 
-    const getHomePage = async (e) => {
-        e.preventDefault();
-        const api_call = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&sort_by=popularity.desc`);
-        const data = await api_call.json();
-        setMovies(data.results);
-        console.log(movies)
-    }
+
     return(
             <Movies movies={movies}/>
     );
