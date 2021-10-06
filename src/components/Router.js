377ab7fe76c6fdda0c5207c01c5072568d03c052
Johanna Salmi Sunnerdahl <73692594+Salmihanna@users.noreply.github.com>
2021-10-06 12:45:10 +0200

    Kommentar

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import App from '../App';
 import Movie from './Movie'
-import Movies from "./Movies";
 
 const Router = () => (
     <BrowserRouter>
         <Switch>
-            {/*TODO: make a search path*/}
             <Route path="/" component={App} exact/>
-            {/*<Route path="/search/" component={Movies} exact/>*/}
             <Route path="/movie/:id" component={Movie}/>
         </Switch>
     </BrowserRouter>
